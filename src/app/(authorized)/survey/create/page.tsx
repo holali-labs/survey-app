16cbbcba77341b0acb5a931d21b669ecf0811233
DaesangYou <eotkd4791@gmail.com>
2025-06-25 23:21:21 +0900

    refactor: 사용하지 않는 메소드 - import 제외

--- src/app/(authorized)/survey/create/page.tsx
+++ src/app/(authorized)/survey/create/page.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
