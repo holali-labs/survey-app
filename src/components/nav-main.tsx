3df00fba33aae2c8b0364973e09101967677be0d
DaesangYou <eotkd4791@gmail.com>
2025-06-25 21:39:00 +0900

    fix(#main): 불필요한 버튼 제거

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -3,13 +3,7 @@
 import { IconPlus, IconFileSmile } from '@tabler/icons-react';
 
 import { Button } from '@/components/ui/button';
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
-} from '@/components/ui/sidebar';
+import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
 
 export function NavMain() {
   return (
@@ -17,10 +11,12 @@ export function NavMain() {
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton tooltip="설문 생성" className="min-w-8 hover:bg-transparent active:bg-transparent">
-              <IconFileSmile />
-              <span>설문 생성</span>
-            </SidebarMenuButton>
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <IconFileSmile />
+                <span>설문 생성</span>
+              </div>
+            </div>
             <Button
               size="icon"
               className="size-8 group-data-[collapsible=icon]:opacity-0 cursor-pointer"
